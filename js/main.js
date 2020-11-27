23aa0078ac826b31f527129817c546fce30d6625
Message Akunna O <message.ao@gmail.com>
2020-11-27 06:15:18 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Create the client, specify the endpoint URL
+client = GraphQL.makeClient("https://api.github.com/graphql"); 
+
+// Set headers to be passed with each request
+// The GitHub API specifies authentication via the Authorization header
+client.setHeader("Authorization", "bearer " + auth_token);  
+
+var query = `
+query {
+  user {
+    login
+  }
+}`;
+
+// Pass the query and a callback to handle the response
+client.query(query, function(response){ 
+  console.log(response); // {"data":{"user":{"login":"nwoodthorpe"}}}
+});
