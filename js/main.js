85e379edda7ab12e53ebeb093935736197de141a
Message Akunna <message.ao@gmail.com>
2020-11-27 09:41:34 +0100

    test

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
+// authentication token
+const github_data = {
+    "token": "d3bb1d48573c99e0e8318298e8b5c53e7a3f046e",
+    "username": "Message-Akunna"
+};
+
 // Create the client, specify the endpoint URL
 client = GraphQL.makeClient("https://api.github.com/graphql"); 
 
-// authentication token
-const auth_token = "d3bb1d48573c99e0e8318298e8b5c53e7a3f046e";
-
 // Set headers to be passed with each request
 // The GitHub API specifies authentication via the Authorization header
-client.setHeader("Authorization", "Message-Akunna" + auth_token);  
+client.setHeader("Authorization", "bearer " +github_data.token);  
 
 var query = `
 query {
-  user(login: "Message-Akunna") {
+  user(login: "${github_data.username}") {
     avatarUrl
     name
     email
@@ -41,5 +44,5 @@ query {
 
 // Pass the query and a callback to handle the response
 client.query(query, function(response){ 
-  console.log(response); // {"data":{"user":{"login":"nwoodthorpe"}}}
+    console.log(response); // {"data":{"user":{"login":"nwoodthorpe"}}}
 });
