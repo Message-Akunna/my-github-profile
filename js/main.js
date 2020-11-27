f376b69aad2fae06ceaff12a1b9a88e526f357c2
Message Akunna <message.ao@gmail.com>
2020-11-27 22:44:39 +0100

    test

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 // authentication token
 const github_data = {
-    "token": "6951f73e15c4aafe76de34456837870288936eef",
+    "token": "278819e55cffc5b9006e319d16c5d6fcc02de932",
     "username": "Message-Akunna"
 };
 
@@ -13,35 +13,36 @@ client = GraphQL.makeClient("https://api.github.com/graphql");
 client.setHeader("Authorization", "bearer " +github_data.token);  
 
 var query = `
-query {
-  user(login: "${github_data.username}") {
-    avatarUrl
-    name
-    email
-    bio
-    repositories(last: 20) {
-      nodes {
-        name
-        description
-        languages(first: 4) {
-          nodes {
-            color
-            name
+query GetAbout {
+    viewer {
+      bio
+      avatarUrl(size: 200)
+      email
+      name
+      repositories(last: 20) {
+        nodes {
+          name
+          description
+          languages(first: 4) {
+            nodes {
+              color
+              name
+            }
           }
+          forks {
+              totalCount
+          }
+          stargazers {
+            totalCount
+          }
+          homepageUrl
+          projectsUrl
+          updatedAt
         }
-        forks {
-          totalCount
-        }
-        stargazers {
-          totalCount
-        }
-        homepageUrl
-        projectsUrl
-        updatedAt
       }
     }
   }
-}`;
+`;
 
 // Pass the query and a callback to handle the response
 client.query(query, function(response){ 
