39b5877bceb05318776c161f08872383c2ffd1ae
Message Akunna <message.ao@gmail.com>
2020-11-29 16:16:06 +0100

    major interface prepared, working on sticky navbar

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// window.onload = function(event) {
+//     const proxyurl = "https://cors-anywhere.herokuapp.com/";
+//     const url = "https://my-github-profile.000webhostapp.com/access.php";
+//     fetch(proxyurl+url)
+//         .then(response => response.json())
+//         .then(data => console.log(data));
+// };
--- js/main.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(event) {
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "https://my-github-profile.000webhostapp.com/access.php";
-    fetch(proxyurl+url)
-        .then(response => response.json())
-        .then(data => console.log(data));
-};
